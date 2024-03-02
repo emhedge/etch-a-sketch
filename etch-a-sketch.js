d2c0fdcd59a13919aeaf42db3efb371fd1eb0de7
emhedge <122066271+emhedge@users.noreply.github.com>
2024-03-02 13:19:07 +0800

    Created box() and track() functions
    Created box() to print debugging info to console.
    log; created track() to monitor mouse location
    within the box (and prints into a temp p for
    debugging purposes)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,35 +8,48 @@ divColumn.setAttribute('class', 'column');
 // for loop with i up to 16
 // each iteration creates a new div and appends it
 // might need distinct functions for row and column
-
 let args = 16
 
 function makeGrid() {
     for (let i = 0; i < args; i++) {
+        
         // creates a row
         // each row has a for loop creating args divs
-        // appends the row
         let newRowItem = document.createElement('div');
         newRowItem.setAttribute('class', 'rowItem');
+
+        // in each row, creates args columns
         for (let i= 0; i < args; i++) {
             let newColItem = document.createElement('div');
             newColItem.setAttribute('class', 'colItem');
             newRowItem.appendChild(newColItem);
         }
+         // appends the row
         divRow.appendChild(newRowItem);        
-        
-
-
     };
 };
-
 makeGrid();
-
-// let newRow = document.createElement('div');
-// newRow = document.setAttribute('class', 'row');
-
-
-// divColumn.appendChild(newRow);
-
-// divColumn.appendChild(divRow);
 container.appendChild(divRow); 
+
+// tracks mouseenter and mouseleave to confirm each box handles events
+let box = document.querySelectorAll('#container');
+for (let i = 0; i < box.length; i++) {
+    box[i].addEventListener('mouseenter', function() {
+        console.log('mouse entered the grid');
+    });
+    box[i].addEventListener('mouseleave', function() {
+    console.log('mouse left the grid');
+});
+}
+
+
+let track = document.querySelectorAll('.colItem');
+for (let i = 0; i < track.length; i++) {
+    track[i].addEventListener('mousemove', (e) => {
+        let log = document.querySelector('#log');
+        log.innerText = `
+        Screen X/Y: (${e.screenX}, ${e.screenY})
+        Client X/Y: (${e.clientX}, ${e.clientY})`
+    });
+}
+
